4e86477dd752b6ced4f3a6d76e69e10d7ee53d52
rafakolle <rafaelakollecarmona@gmail.com>
2024-03-18 09:56:14 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -1,6 +1,21 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar);
+var nome = document.getElementById("nome");
 
+var saida = document.getElementById("saida-de-dados");
+
+var email = document.getElementById("email");
 function alertar (){
-    alert("Você clicou no botão!");
+    //alert (nome.value + " " + " clicou no botão!!!")
+   saida.innerText = "Nome " + " " + nome.value +
+   "\n E-mail: "+ " " + email.value;
+     SVGAnimatedAngle.innerText = "nome" + " " + nome.value 
+}
+
+var email = document.getElementById("email");
+function alertar (){
+    //alert (nome.value + " " + " clicou no botão!!!")
+   saida.innerText = "Nome " + " " + nome.value +
+   "\n E-mail: "+ " " + email.value;
+     SVGAnimatedAngle.innerText = "nome" + " " + nome.value 
 }
